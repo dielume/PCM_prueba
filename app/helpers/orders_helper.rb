7ff6de34d6c4bc89bfca8d5689092887c296ef74
Diego Lume <diego.lume.a@gmail.com>
2018-05-07 00:36:20 -0500

    edit added for orders view

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,17 @@
 module OrdersHelper
+  def food_id_exist?(food, food_orders)
+    food_orders.each do |food_order|
+      if food_order.food_id == food.id
+        return true
+      end
+    end
+    false
+  end
+  def food_order_quantity(food, food_orders)
+    food_orders.each do |food_order|
+      if food_order.food_id == food.id
+        return food_order.quantity
+      end
+    end
+  end
 end
