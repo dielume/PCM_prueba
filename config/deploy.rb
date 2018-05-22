9aa3a7b213c99ee7b156ee8e3fbad434590b725d
Diego Lume <diego.lume.a@gmail.com>
2018-05-21 21:33:10 -0500

    change file

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :rbenv_roles, :all # default value
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/application.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
